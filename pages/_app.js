0d7245677f69a6d65faf3cf6c4156dd601b90206
Wyatt Ades <wyattades@gmail.com>
2021-12-28 15:27:16 -0800

    add page title

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Script from "next/script";
+import Head from "next/head";
 
 import "styles/global.scss";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Minesweeper Online</title>
+      </Head>
+
       {process.env.NODE_ENV === "production" ? (
         <Script
           data-website-id="fc60dcb4-ed21-4f00-8964-f078eebcaa2b"
